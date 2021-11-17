686444c3c44f4b53c40b17b875ff71ec819fd614
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:19:04 +0200

    BlackSheep-week12

---
+++ RandomQuote/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\opilane\samples\RandomQuotes.txt";
+            string[] datafromfile = File.ReadAllLines(path);
+
+            Console.WriteLine(datafromfile[0]);
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(1, datafromfile.Length);
+            Console.WriteLine(datafromfile[randomIndex]);
+        }
+    }
+}
---
+++ Seasons/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string autumnpath = @"C:\Users\opilane\samples\Autumn.txt";
+            string winterpath = @"C:\Users\opilane\samples\Winter.txt";
+            string[] dataFromAutumn = File.ReadAllLines(autumnpath);
+            string[] dataFromWinter = File.ReadAllLines(winterpath);
+
+            File.WriteAllLines(autumnpath, dataFromWinter);
+            File.WriteAllLines(winterpath, dataFromAutumn);
+            
+        }
+    }
+}
